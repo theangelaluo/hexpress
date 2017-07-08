26c6eb8e6580c6bba6cbf3ff7cd7759fd71ec997
Moose Paksoy <mustafa@joinhorizons.com>
2017-07-08 11:23:48 -0700

    Initial commit

---
+++ hexpress.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = function () {
+  // YOUR CODE HERE
+
+  return {
+    get: function(route, callback) {
+      // YOUR GET METHOD HERE
+    }
+    // YOUR METHODS HERE
+  };
+};
