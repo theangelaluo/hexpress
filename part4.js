49d3e43b6e62919243e9e42671351a9d022da6f2
Moose Paksoy <mustafa@joinhorizons.com>
2017-07-11 21:25:12 -0700

    Tweak part 4 test cases

--- part4.js
+++ part4.js
@@ -5,14 +5,14 @@ var app = hexpress();
 
 // Simple route '/' should render the index.hbs file
 // located in /hexpress/views. Should replace {{fname}}
-// and {{lname}} with Prath and Desai respectively.
+// and {{lname}} with Pierre and Trudeau respectively.
 // Additionally it should list all artists listed in the
 // favouriteArtists array
 app.get('/', function(req, res) {
-  res.render('./views/index.hbs', {
+  res.render('index.hbs', {
     fname: 'Pierre',
     lname: 'Trudeau',
-    favouriteArtists: ['Taylor Swift', 'Seafret', 'The Killers', 'Bon Iver', 'Bob Dylan', 'Young the Giant']
+    artists: ['Taylor Swift', 'Seafret', 'The Killers', 'Bon Iver', 'Bob Dylan', 'Young the Giant']
   });
 });
 
